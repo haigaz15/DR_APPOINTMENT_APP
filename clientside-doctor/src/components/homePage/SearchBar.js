366b7bbc8490a57f8da765029daba851fc653335
haigaz15 <haigazsoghomonian@gmail.com>
2022-12-21 21:34:55 +0400

    Demo/starter page for doctors'

---
+++ clientside-doctor/src/components/homePage/SearchBar.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+
+const SearchBar = () => {
+  return (
+    <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div>
+        <FormControl  sx={{ m: 1,width:'500px' }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+          <Input
+            id="standard-adornment-amount"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          />
+        </FormControl>
+      </div>
+    </Box>
+  );
+}
+
+export default SearchBar
